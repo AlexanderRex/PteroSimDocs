c3fc11b4cbdd852c3f25dc7902305d4a5c93ceb0
Yakunin Gleb <yakuningleb6@gmail.com>
2026-02-21 14:07:31 +0300

    removed placeholder files

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'PteroSim'
+copyright = '2024, AlexanderRex'
+author = 'AlexanderRex'
 
 release = '0.1'
 version = '0.1.0'
